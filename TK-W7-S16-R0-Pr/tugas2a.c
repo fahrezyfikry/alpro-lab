c1ecbda4f533388cf5582b8a3656cd433ec53c78
fahrezyfikry <fahrezyfikry@gmail.com>
2025-07-15 17:19:52 +0700

    Update tugas2a.c

--- TK-W7-S16-R0-Pr/tugas2a.c
+++ TK-W7-S16-R0-Pr/tugas2a.c
@@ -1,5 +1,27 @@
 #include <stdio.h>
 
 int main() {
-  return 0;
+    printf("Enter payment amount: ");
+
+    float payment;
+    scanf("%f", &payment);
+
+    int discount_multiplier = 5;
+    int discount_base = 100000;
+    int discount_coupon = payment / discount_base;
+    printf("Discount Coupon QTY: %d\n", discount_coupon);
+
+    int discount = discount_coupon * discount_multiplier;
+    printf("Discount: %d\n", discount);
+
+    float discount_percentage = (float) discount / (float) 100;
+    printf("Discount Percentage: %.2f\n", discount_percentage);
+
+    float discount_amount = (payment * discount_percentage);
+    printf("Discount amount: %.2f\n", discount_amount);
+
+    float final_payment = payment - discount_amount;
+    printf("Final payment amount: %.2f\n", final_payment);
+
+    return 0;
 }
